673e38cc032408a3ab70d1e11190a02c852789b3
Aayush Acharya <acharyaaayush2k4@gmail.com>
2026-01-27 07:20:38 +0545

    Re Arranged

---
+++ JAVA/javac1/src/main/java/com/mycompany/javac1/Javac1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javac1;
+
+/**
+ *
+ * @author N I T R O
+ */
+public class Javac1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+ 
